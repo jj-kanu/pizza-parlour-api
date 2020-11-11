7c9657fadd9a6b0f831376fbc82a24c743b69789
Aaron Li <aaronhf.li@mail.utoronto.ca>
2020-11-11 18:37:36 -0500

    Extract methods to make helper functions for cleaner code

--- main.py
+++ main.py
@@ -33,11 +33,7 @@ def accept_input(main_menu_input):
             pizza_option = input("What type of pizza would you like? ")
         # PREMADE PIZZAS
         if int(pizza_option) in range(1, 4):
-            print("Enter 1 for Small, 2 for Medium, 3 for Large, 4 for Party Size.")
-            size_option = input("What size pizza do you want? ")
-            while int(size_option) not in range(1, 5):
-                print("Invalid size. Please enter a valid pizza size flag.")
-                size_option = input("What size pizza do you want? ")
+            size_option = choose_pizza_size()
             url_string = "http://127.0.0.1:5000/choose-pizza/" + \
                 pizza_option + "/" + size_option
             response = requests.post(url_string)
@@ -45,33 +41,11 @@ def accept_input(main_menu_input):
         # CUSTOM PIZZAS
         elif int(pizza_option) == 4:
             # Dough
-            print("Enter 1 for White, 2 for Whole Wheat, 3 for Cauliflower")
-            dough_option = input("What type of dough would you like? ")
-            while int(dough_option) not in range(1, 4):
-                print("Invalid dough type. Please enter a valid dough type flag.")
-                dough_option = input("What type of dough would you like? ")
+            dough_option = custom_choose_dough()
             # Toppings
-            print("Enter Topping Number: 1: pepperoni, 2:bacon, 3:pineapple, 4:chicken,\n \
-                5: bell peppers, 6: jalapeno peppers")
-            print("Enter 0 to Stop Adding Toppings")
-            list_of_toppings = ""
-            topping_option = -1
-            while int(topping_option) != 0:
-                topping_option = input("What toppings do you want to add? ")
-                if int(topping_option) in range(1, 7):
-                    list_of_toppings += str(topping_option) + ","
-                    print("Topping added.")
-                elif int(topping_option) == 0:
-                    list_of_toppings = list_of_toppings[:-1]
-                    print("Nice choice!")
-                else:
-                    print("Please enter a topping number or press 0 to stop.")
+            list_of_toppings = custom_choose_topping()
             # Size
-            print("Enter 1 for Small, 2 for Medium, 3 for Large, 4 for Party Size.")
-            size_option = input("What size pizza do you want? ")
-            while int(size_option) not in range(1, 5):
-                print("Invalid size. Please enter a valid pizza size flag.")
-                size_option = input("What size pizza do you want? ")
+            size_option = choose_pizza_size()
 
             url_string = "http://127.0.0.1:5000/create-pizza/" + \
                 dough_option + "/" + list_of_toppings + "/" + size_option
@@ -80,18 +54,7 @@ def accept_input(main_menu_input):
 
     # Remove Pizza
     if main_menu_input == "2":
-        client_view_cart()
-
-        pizza_id = input("What is the id of the pizza you want to remove? ")
-        url_string = "http://127.0.0.1:5000/is-pizza-in-cart/" + pizza_id
-        response = requests.post(url_string)
-        # Checks if Pizza is in cart
-        if response.text == "":
-            url_string = "http://127.0.0.1:5000/remove-pizza/" + pizza_id
-            requests.post(url_string)
-            print("Pizza removed from cart.")
-        else:
-            print(response.text)
+        remove_pizza()
 
     # Edit Pizza
     if main_menu_input == "3":
@@ -111,71 +74,18 @@ def accept_input(main_menu_input):
                     "What would you like to change about your pizza? ")
                 # Edit Dough
                 if int(edit_flag) == 1:
-                    print(
-                        "Pizza Dough Number: 1 = White, 2 = Whole Wheat, 3 = Cauliflower")
-                    dough = input("What dough would you like? ")
-                    while int(dough) not in range(1, 4):
-                        print("Please enter a valid dough type number.")
-                        dough = input("What dough would you like? ")
-                    url_string = "http://127.0.0.1:5000/change-pizza-dough/" + pizza_id + "/" + dough
-                    response = requests.post(url_string)
-                    print(response.text)
+                    edit_dough(pizza_id)
                 # Add Toppings
                 elif int(edit_flag) == 2:
-                    print("Enter Topping Number: 1: pepperoni, 2:bacon, 3:pineapple, 4:chicken,\n \
-                        5: bell peppers, 6: jalapeno peppers")
-                    print("Enter 0 to Stop Adding Toppings")
-                    list_of_toppings = ""
-                    topping_option = -1
-                    while int(topping_option) != 0:
-                        topping_option = input(
-                            "What toppings do you want to add? ")
-                        if int(topping_option) in range(1, 7):
-                            list_of_toppings += str(topping_option) + ","
-                            print("Topping added.")
-                        elif int(topping_option) == 0:
-                            list_of_toppings = list_of_toppings[:-1]
-                        else:
-                            print(
-                                "Please enter a valid topping number or press 0 to stop.")
-                    url_string = "http://127.0.0.1:5000/add-topping-to-pizza/" + \
-                        pizza_id + "/" + list_of_toppings
-                    response = requests.post(url_string)
-                    print(response.text)
+                    edit_toppings(pizza_id)
 
                 # Remove Toppings
                 elif int(edit_flag) == 3:
-                    print("Enter Topping Number: 1: pepperoni, 2:bacon, 3:pineapple, 4:chicken,\n \
-                        5: bell peppers, 6: jalapeno peppers")
-                    print("Enter 0 to Stop Removing Toppings")
-                    list_of_toppings = ""
-                    topping_option = -1
-                    while int(topping_option) != 0:
-                        topping_option = input(
-                            "What toppings do you want to remove? ")
-                        if int(topping_option) in range(1, 7):
-                            list_of_toppings += str(topping_option) + ","
-                            print("Topping Removed.")
-                        elif int(topping_option) == 0:
-                            list_of_toppings = list_of_toppings[:-1]
-                        else:
-                            print(
-                                "Please enter a valid topping number or press 0 to stop.")
-                    url_string = "http://127.0.0.1:5000/remove-topping-from-pizza/" + \
-                        pizza_id + "/" + list_of_toppings
-                    response = requests.post(url_string)
-                    print(response.text)
+                    remove_toppings(pizza_id)
 
-                    # remove Toppings
+                # Edit size
                 elif int(edit_flag) == 4:
-                    print("Pizza Sizes: 1 = Small, 2 = Medium, 3 = Large, 4 = Party")
-                    size = input("What size would you like? ")
-                    while int(size) not in range(1, 5):
-                        print("Please enter a valid size number.")
-                        size = input("What size would you like? ")
-                    url_string = "http://127.0.0.1:5000/change-pizza-size/" + pizza_id + "/" + size
-                    response = requests.post(url_string)
-                    print(response.text)
+                    edit_size(pizza_id)
                 else:
                     print(
                         "Please pick a valid edit option or enter 0 to go back to menu.")
@@ -202,53 +112,13 @@ def accept_input(main_menu_input):
         client_clear_cart()
 
     if main_menu_input == "8":
-        response = requests.get("http://127.0.0.1:5000/view-menu")
-        print(response.text + "\n")
+        view_menu()
 
     if main_menu_input == "9":
-        order_item = input("What item would you like to look up? ")
-        url_string = "http://127.0.0.1:5000/parse-menu/" + order_item
-        response = requests.post(url_string)
-        print(response.text)
+        parse_menu()
 
     if main_menu_input == "10":
-        client_view_cart()
-        confirmation = input(
-            "Are you sure you want to check out now?(Enter \'yes\' or \'no\') ")
-        while confirmation.lower() != "no" and confirmation.lower() != "yes":
-            confirmation = input(
-                "Are you sure you want to check out now?(Enter \'yes\' or \'no\') ")
-        if confirmation.lower() == "no":
-            print("Maybe order some more?")
-        else:
-            response = requests.get("http://127.0.0.1:5000/is-cart-empty")
-            if response.text == "":
-                print("Choose Delivery Method:")
-                print(
-                    "1: In-Store Pickup, 2: In-House Delivery, 3: Foodora, 4: Uber-Eats")
-                delivery_choice = input(
-                    "How would you like to get your food? ")
-                if int(delivery_choice) == 1:
-                    print("Your order will be ready for pickup in 20 minutes.")
-                else:
-                    address = input("Enter your address: ")
-                    if int(delivery_choice) == 2:
-                        url_string = "http://127.0.0.1:5000/csv-generation/" + address
-                        response = requests.post(url_string)
-                        print(response.text)
-                        print("Order Info sent to Delivery Man in CSV format")
-                    if int(delivery_choice) == 3:
-                        url_string = "http://127.0.0.1:5000/csv-generation/" + address
-                        response = requests.post(url_string)
-                        print(response.text)
-                        print("Order Info sent to Foodora in CSV format")
-                    if int(delivery_choice) == 4:
-                        url_string = "http://127.0.0.1:5000/json-generation/" + address
-                        response = requests.post(url_string)
-                        print(response.text)
-                        print("Order Info sent to UberEats in JSON format")
-            else:
-                print(response.text)
+        checkout()
 
     if main_menu_input == "98":
         client_view_cart()
@@ -277,11 +147,188 @@ def accept_input(main_menu_input):
         response = requests.post(
             "http://127.0.0.1:5000/are-there-drinks-in-cart")
         if response.text == "":
-            # Do stuff
             return
         else:
             print(response.text)
     return main_menu_input
+
+
+def choose_pizza_size():
+    print("Enter 1 for Small, 2 for Medium, 3 for Large, 4 for Party Size.")
+    size_option = input("What size pizza do you want? ")
+    while int(size_option) not in range(1, 5):
+        print("Invalid size. Please enter a valid pizza size flag.")
+        size_option = input("What size pizza do you want? ")
+    return size_option
+
+
+def custom_choose_topping():
+    print("Enter Topping Number: 1: pepperoni, 2:bacon, 3:pineapple, 4:chicken,\n \
+                5: bell peppers, 6: jalapeno peppers")
+    print("Enter 0 to Stop Adding Toppings")
+    list_of_toppings = ""
+    topping_option = -1
+    while int(topping_option) != 0:
+        topping_option = input("What toppings do you want to add? ")
+        if int(topping_option) in range(1, 7):
+            list_of_toppings += str(topping_option) + ","
+            print("Topping added.")
+        elif int(topping_option) == 0:
+            list_of_toppings = list_of_toppings[:-1]
+            print("Nice choice!")
+        else:
+            print("Please enter a topping number or press 0 to stop.")
+    return list_of_toppings
+
+
+def custom_choose_dough():
+    print("Enter 1 for White, 2 for Whole Wheat, 3 for Cauliflower")
+    dough_option = input("What type of dough would you like? ")
+    while int(dough_option) not in range(1, 4):
+        print("Invalid dough type. Please enter a valid dough type flag.")
+        dough_option = input("What type of dough would you like? ")
+    return dough_option
+
+
+def remove_pizza():
+    client_view_cart()
+    pizza_id = input("What is the id of the pizza you want to remove? ")
+    url_string = "http://127.0.0.1:5000/is-pizza-in-cart/" + pizza_id
+    response = requests.post(url_string)
+    # Checks if Pizza is in cart
+    if response.text == "":
+        url_string = "http://127.0.0.1:5000/remove-pizza/" + pizza_id
+        requests.post(url_string)
+        print("Pizza removed from cart.")
+    else:
+        print(response.text)
+
+
+def edit_size(pizza_id):
+    print("Pizza Sizes: 1 = Small, 2 = Medium, 3 = Large, 4 = Party")
+    size = input("What size would you like? ")
+    while int(size) not in range(1, 5):
+        print("Please enter a valid size number.")
+        size = input("What size would you like? ")
+    url_string = "http://127.0.0.1:5000/change-pizza-size/" + pizza_id + "/" + size
+    response = requests.post(url_string)
+    print(response.text)
+
+
+def remove_toppings(pizza_id):
+    print("Enter Topping Number: 1: pepperoni, 2:bacon, 3:pineapple, 4:chicken,\n \
+                        5: bell peppers, 6: jalapeno peppers")
+    print("Enter 0 to Stop Removing Toppings")
+    list_of_toppings = ""
+    topping_option = -1
+    while int(topping_option) != 0:
+        topping_option = input(
+            "What toppings do you want to remove? ")
+        if int(topping_option) in range(1, 7):
+            list_of_toppings += str(topping_option) + ","
+            print("Topping Removed.")
+        elif int(topping_option) == 0:
+            list_of_toppings = list_of_toppings[:-1]
+        else:
+            print(
+                "Please enter a valid topping number or press 0 to stop.")
+    url_string = "http://127.0.0.1:5000/remove-topping-from-pizza/" + \
+                 pizza_id + "/" + list_of_toppings
+    response = requests.post(url_string)
+    print(response.text)
+
+
+def edit_toppings(pizza_id):
+    print("Enter Topping Number: 1: pepperoni, 2:bacon, 3:pineapple, 4:chicken,\n \
+                        5: bell peppers, 6: jalapeno peppers")
+    print("Enter 0 to Stop Adding Toppings")
+    list_of_toppings = ""
+    topping_option = -1
+    while int(topping_option) != 0:
+        topping_option = input(
+            "What toppings do you want to add? ")
+        if int(topping_option) in range(1, 7):
+            list_of_toppings += str(topping_option) + ","
+            print("Topping added.")
+        elif int(topping_option) == 0:
+            list_of_toppings = list_of_toppings[:-1]
+        else:
+            print(
+                "Please enter a valid topping number or press 0 to stop.")
+    url_string = "http://127.0.0.1:5000/add-topping-to-pizza/" + \
+                 pizza_id + "/" + list_of_toppings
+    response = requests.post(url_string)
+    print(response.text)
+
+
+def edit_dough(pizza_id):
+    print(
+        "Pizza Dough Number: 1 = White, 2 = Whole Wheat, 3 = Cauliflower")
+    dough = input("What dough would you like? ")
+    while int(dough) not in range(1, 4):
+        print("Please enter a valid dough type number.")
+        dough = input("What dough would you like? ")
+    url_string = "http://127.0.0.1:5000/change-pizza-dough/" + pizza_id + "/" + dough
+    response = requests.post(url_string)
+    print(response.text)
+
+
+def view_menu():
+    response = requests.get("http://127.0.0.1:5000/view-menu")
+    print(response.text + "\n")
+
+
+def parse_menu():
+    order_item = input("What item would you like to look up? ")
+    url_string = "http://127.0.0.1:5000/parse-menu/" + order_item
+    response = requests.post(url_string)
+    print(response.text)
+
+
+def checkout():
+    client_view_cart()
+    confirmation = input(
+        "Are you sure you want to check out now?(Enter \'yes\' or \'no\') ")
+    while confirmation.lower() != "no" and confirmation.lower() != "yes":
+        confirmation = input(
+            "Are you sure you want to check out now?(Enter \'yes\' or \'no\') ")
+    if confirmation.lower() == "no":
+        print("Maybe order some more?")
+    else:
+        choose_delivery_method()
+        checkout_complete()
+        exit()
+
+
+def choose_delivery_method():
+    response = requests.get("http://127.0.0.1:5000/is-cart-empty")
+    if response.text == "":
+        print("Choose Delivery Method:")
+        print(
+            "1: In-Store Pickup, 2: In-House Delivery, 3: Foodora, 4: Uber-Eats")
+        delivery_choice = input(
+            "How would you like to get your food? ")
+        if int(delivery_choice) == 1:
+            print("Your order will be ready for pickup in 20 minutes.")
+        else:
+            address = input("Enter your address: ")
+            if int(delivery_choice) == 2:
+                url_string = "http://127.0.0.1:5000/csv-generation/" + address
+                response = requests.post(url_string)
+                print(response.text)
+                print("Order Info sent to Delivery Man in CSV format")
+            if int(delivery_choice) == 3:
+                url_string = "http://127.0.0.1:5000/csv-generation/" + address
+                response = requests.post(url_string)
+                print(response.text)
+                print("Order Info sent to Foodora in CSV format")
+            if int(delivery_choice) == 4:
+                url_string = "http://127.0.0.1:5000/json-generation/" + address
+                response = requests.post(url_string)
+                print(response.text)
+                print("Order Info sent to UberEats in JSON format")
+    else:
+        print(response.text)
 
 
 def is_price_float(new_price):
@@ -319,6 +366,9 @@ def client_add_drinks():
         drink_option + "/" + drink_quantity
     return url_string
 
+def checkout_complete():
+    response = requests.get("http://127.0.0.1:5000/complete-order")
+    print(response.text)
 
 if __name__ == "__main__":
     main()
